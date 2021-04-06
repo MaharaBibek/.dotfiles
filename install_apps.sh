c0f6a63a87db2fccf337781eae0afd600771885f
Mahara,Bibek <bbekmahara@gmail.com>
2021-04-06 15:27:58 +0545

    EDIT/ADDITION(.sh SCRIPT): Vim PLUGINs (ale & vim-gitgutter)

--- install_apps.sh
+++ install_apps.sh
@@ -4,10 +4,19 @@ sudo apt-get install git vim tmux feh
 
 # Install Vim-Plugin
 mkdir ~/.vim/bundle && cd ~/.vim/bundle
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/powerline/powerline 
+git clone https://github.com/kien/ctrlp.vim.git 
+git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
 git clone https://github.com/python-mode/python-mode.git
 git clone https://github.com/vimwiki/vimwiki.git
+
+# ALE - Asynchronous Lint Engine
+git clone https://github.com/dense-analysis/ale.git
+
+# Install vim-gitgutter
+mkdir -p ~/.vim/pack/airblade/start
+cd ~/.vim/pack/airblade/start
+git clone https://github.com/airblade/vim-gitgutter.git
+vim -u NONE -c "helptags vim-gitgutter/doc" -c q
 
 # sudo apt-get install binwalk feh nitrogen dmenu polybar vifm ranger 
 
