12d5d83064d101bc5ee7230cef663a02c12fe93e
Mahara,Bibek <bbekmahara@gmail.com>
2021-04-21 19:23:38 +0545

    install_apps.sh: RE-ARRANGED

--- install_apps.sh
+++ install_apps.sh
@@ -4,20 +4,26 @@ sudo apt-get install git vim tmux feh
 
 # Install Vim-Plugins
 mkdir ~/.vim/bundle && cd ~/.vim/bundle
+
 # PLUGIN: VimWiki
 git clone https://github.com/vimwiki/vimwiki.git
-# PLUGIN: StatusBar 
+
+# PLUGIN: StatusBar (Lightline)
 git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
-# PLUGIN: Limelight
+
+# PLUGIN: EAGLE-VIEW FOCUS (Limelight, Goyo)
 git clone https://github.com/junegunn/limelight.vim.git
-# PLUGIN: Goyo
 git clone https://github.com/junegunn/goyo.vim.git
-# PLUGIN: Search
+
+# PLUGIN: Search (Ctrlp)
 git clone https://github.com/kien/ctrlp.vim.git 
-# PY PLUGIN: python-mode
+
+# .py PLUGIN: python-mode
 git clone --recurse-submodules https://github.com/python-mode/python-mode.git
+
 # PLUGIN: ALE - Asynchronous Lint Engine
 git clone https://github.com/dense-analysis/ale.git
+
 # PLUGIN: vim-gitgutter
 mkdir -p ~/.vim/pack/airblade/start
 cd ~/.vim/pack/airblade/start
